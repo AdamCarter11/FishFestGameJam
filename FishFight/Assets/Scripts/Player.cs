c06ad51d7c5a3c69e3798fc0515989fe84cf1a77
Adam <57573881+AdamCarter11@users.noreply.github.com>
2024-04-03 17:21:30 -0700

    More work on gamemanager

--- FishFight/Assets/Scripts/Player.cs
+++ FishFight/Assets/Scripts/Player.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] int health = 5;
+    [SerializeField] KeyCode actionOne, actionTwo, actionThree, actionFour;
+    // [SerializeField] List<> options; // will hold a list of the SO type
+
+    // last actions used (data type will be the SO)
+    // currently selected action (data type will be the SO)
+
+    private gamemanager gmRef;
+    private int currHealth;
+    private bool selected = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gmRef = GameObject.FindGameObjectWithTag("GM").GetComponent<gamemanager>();
+        currHealth = health;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (gmRef.currentState)
+        {
+            case GameState.FirstSelect:
+                if (Input.GetKeyDown(actionOne))
+                {
+                    selected = true;
+                    // currently selected action = the option in the first position
+                }
+                break;
+        }
     }
+
+
+    #region helper functions
+    public int GetHealth()
+    {
+        return currHealth;
+    }
+    public void ResetPlayer()
+    {
+        currHealth = health;
+        // set last actions used to null
+    }
+    #endregion
 }
--- FishFight/Assets/Scripts/gamemanager.cs
+++ FishFight/Assets/Scripts/gamemanager.cs
@@ -14,5 +14,14 @@ public enum GameState
 public class gamemanager : MonoBehaviour
 {
     [HideInInspector] public GameState currentState;
+    [Tooltip("if we want to force a tie after this many rounds")] [SerializeField] int maxRounds = 999;
+    [SerializeField] Player playerOne, playerTwo;
+    int currRound = 0;
 
+    public void ResetGame()
+    {
+        currRound = 0;
+        playerOne.ResetPlayer();
+        playerTwo.ResetPlayer();
+    }
 }
