f109e06f831c83ae690d13ecee16aaa4318fb02a
Adam <57573881+AdamCarter11@users.noreply.github.com>
2024-04-04 18:16:45 -0700

    Basic actions + display setup

--- FishFight/Assets/Scripts/Player.cs
+++ FishFight/Assets/Scripts/Player.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] int whichChar = 0;
     [SerializeField] int health = 5;
-    [SerializeField] KeyCode actionOne, actionTwo, actionThree, actionFour;
+    [SerializeField] KeyCode actionOne, actionTwo, actionThree;
+    [SerializeField] TextMeshProUGUI optionOneText, optionTwoText, optionThreeText;
     // [SerializeField] List<> options; // will hold a list of the SO type
 
     // last actions used (data type will be the SO)
@@ -15,11 +18,17 @@ public class Player : MonoBehaviour
     private int currHealth;
     private bool selected = false;
 
+    List<string> actionList = new List<string>() { "quickAttack", "strongAttack", "counter", "parry", "maneuver"};
+    List<string> optionsList = new List<string>();
+
+    List<string> tempDisabled = new List<string> ();
+
     // Start is called before the first frame update
     void Start()
     {
         gmRef = GameObject.FindGameObjectWithTag("GM").GetComponent<gamemanager>();
         currHealth = health;
+        GenerateOptionsHelper();
     }
 
     // Update is called once per frame
@@ -30,11 +39,74 @@ public class Player : MonoBehaviour
             case GameState.FirstSelect:
                 if (Input.GetKeyDown(actionOne))
                 {
-                    selected = true;
-                    // currently selected action = the option in the first position
+                    gmRef.SetAction(optionsList[0], whichChar);
+                }
+                if (Input.GetKeyDown(actionTwo))
+                {
+                    gmRef.SetAction(optionsList[1], whichChar);
+                }
+                if (Input.GetKeyDown(actionThree))
+                {
+                    gmRef.SetAction(optionsList[2], whichChar);
+                }
+                break;
+            case GameState.SecondSelect:
+                if (Input.GetKeyDown(actionOne) && optionsList[0] != tempDisabled[0])
+                {
+                    gmRef.SetAction(optionsList[0], whichChar);
+                }
+                if (Input.GetKeyDown(actionTwo) && optionsList[1] != tempDisabled[0])
+                {
+                    gmRef.SetAction(optionsList[1], whichChar);
+                }
+                if (Input.GetKeyDown(actionThree) && optionsList[2] != tempDisabled[0])
+                {
+                    gmRef.SetAction(optionsList[2], whichChar);
                 }
                 break;
         }
+    }
+
+    List<string> GetUniqueRandomActions(int count = -1)
+    {
+        if(count < 0)
+            count = 3;
+
+        List<string> uniqueActions = new List<string>();
+        List<string> actionsLeft = new List<string>(actionList);
+        if(tempDisabled.Count > 0)
+        {
+            int x = 0;
+            for (int i = 0; i < actionsLeft.Count; ++i)
+            {
+                if (actionsLeft[i] == tempDisabled[x])
+                {
+                    actionsLeft.RemoveAt(i);
+                    x++;
+                    if (x >= tempDisabled.Count)
+                        break;
+                }
+            }
+            tempDisabled.Clear();
+        }
+        List<string> availableActions = new List<string>(actionsLeft);
+
+        for (int i = 0; i < count; i++)
+        {
+            int index = Random.Range(0, availableActions.Count);
+            uniqueActions.Add(availableActions[index]);
+            availableActions.RemoveAt(index);
+        }
+
+        optionOneText.gameObject.SetActive(true);
+        optionTwoText.gameObject.SetActive(true);
+        optionThreeText.gameObject.SetActive(true);
+
+        optionOneText.text = uniqueActions[0];
+        optionTwoText.text = uniqueActions[1];
+        optionThreeText.text = uniqueActions[2];
+
+        return uniqueActions;
     }
 
 
@@ -43,10 +115,30 @@ public class Player : MonoBehaviour
     {
         return currHealth;
     }
+    public void ChangeHealth(int changeVal)
+    {
+        currHealth -= changeVal;
+    }
     public void ResetPlayer()
     {
         currHealth = health;
+        tempDisabled.Clear();
+        GenerateOptionsHelper();
         // set last actions used to null
+    }
+    public void AddToDisabled(string disabledString)
+    {
+        tempDisabled.Add(disabledString);
+        if (optionOneText.text == disabledString)
+            optionOneText.gameObject.SetActive(false);
+        if (optionTwoText.text == disabledString)
+            optionTwoText.gameObject.SetActive(false);
+        if (optionThreeText.text == disabledString)
+            optionThreeText.gameObject.SetActive(false);
+    }
+    public void GenerateOptionsHelper()
+    {
+        optionsList = GetUniqueRandomActions();
     }
     #endregion
 }
--- FishFight/Assets/Scripts/gamemanager.cs
+++ FishFight/Assets/Scripts/gamemanager.cs
@@ -18,10 +18,238 @@ public class gamemanager : MonoBehaviour
     [SerializeField] Player playerOne, playerTwo;
     int currRound = 0;
 
+    string playerOneAction = "", playerTwoAction = "";
+    bool gameOver = false;
+
+    private void Update()
+    {
+        if (!gameOver)
+        {
+            StateLogic();
+        }
+    }
+
+    private void StateLogic()
+    {
+        switch (currentState)
+        {
+            case GameState.FirstSelect:
+                if (playerOneAction != "" && playerTwoAction != "")
+                {
+                    CheckOutCome();
+                    // make sure to set exhausted cards
+                    playerOne.AddToDisabled(playerOneAction);
+                    playerTwo.AddToDisabled(playerTwoAction);
+                    playerOneAction = "";
+                    playerTwoAction = "";
+
+                    print("resolved FIRST select");
+
+                    currentState = GameState.ResolutionOne;
+                }
+                break;
+            case GameState.ResolutionOne:
+                CheckHealth();
+
+                print("resolved FIRST resolution");
+
+                currentState = GameState.SecondSelect;
+                break;
+            case GameState.SecondSelect:
+                if (playerOneAction != "" && playerTwoAction != "")
+                {
+                    CheckOutCome();
+                    // make sure to set exhausted cards
+                    playerOne.AddToDisabled(playerOneAction);
+                    playerTwo.AddToDisabled(playerTwoAction);
+                    playerOneAction = "";
+                    playerTwoAction = "";
+
+                    print("resolved SECOND select");
+
+                    currentState = GameState.ResolutionTwo;
+                }
+                break;
+            case GameState.ResolutionTwo:
+                CheckHealth();
+
+                print("resolved SECOND resolution");
+
+                currentState = GameState.ResetHand;
+                break;
+            case GameState.ResetHand:
+                playerOne.GenerateOptionsHelper();
+                playerTwo.GenerateOptionsHelper();
+
+                print("resolved reset hand");
+
+                currentState = GameState.FirstSelect;
+                break;
+        }
+    }
+
     public void ResetGame()
     {
         currRound = 0;
         playerOne.ResetPlayer();
         playerTwo.ResetPlayer();
     }
+
+    public void SetAction(string newAction, int whichChar)
+    {
+        if (whichChar == 0)
+            playerOneAction = newAction;
+        if (whichChar == 1)
+            playerTwoAction = newAction;
+
+        //print("1: " + playerOneAction + " 2: " + playerTwoAction);
+    }
+
+    #region State checks
+    private void CheckHealth()
+    {
+        if (playerOne.GetHealth() <= 0 && playerTwo.GetHealth() <= 0)
+        {
+            // both players lose
+            print("TIE");
+            gameOver = true;
+        }
+        else if (playerOne.GetHealth() <= 0)
+        {
+            // player two wins
+            print("player two wins");
+            gameOver = true;
+        }
+        else if (playerTwo.GetHealth() <= 0)
+        {
+            // player one wins
+            print("player one wins");
+            gameOver = true;
+        }
+    }
+    private void CheckOutCome()
+    {
+        if (playerOneAction == "quickAttack" && playerTwoAction == "quickAttack")
+        {
+            playerOne.ChangeHealth(1);
+            playerTwo.ChangeHealth(1);
+        }
+        if (playerOneAction == "quickAttack" && playerTwoAction == "strongAttack")
+        {
+            playerOne.ChangeHealth(2);
+            playerTwo.ChangeHealth(1);
+        }
+        if (playerOneAction == "strongAttack" && playerTwoAction == "quickAttack")
+        {
+            playerOne.ChangeHealth(1);
+            playerTwo.ChangeHealth(2);
+        }
+        if (playerOneAction == "strongAttack" && playerTwoAction == "strongAttack")
+        {
+            playerOne.ChangeHealth(2);
+            playerTwo.ChangeHealth(2);
+        }
+        if (playerOneAction == "quickAttack" && playerTwoAction == "counter")
+        {
+            playerOne.ChangeHealth(1);
+            playerTwo.ChangeHealth(0);
+        }
+        if (playerOneAction == "counter" && playerTwoAction == "quickAttack")
+        {
+            playerOne.ChangeHealth(0);
+            playerTwo.ChangeHealth(1);
+        }
+        if (playerOneAction == "quickAttack" && playerTwoAction == "parry")
+        {
+            playerOne.ChangeHealth(0);
+            playerTwo.ChangeHealth(0);
+        }
+        if (playerOneAction == "parry" && playerTwoAction == "quickAttack")
+        {
+            playerOne.ChangeHealth(0);
+            playerTwo.ChangeHealth(0);
+        }
+        if (playerOneAction == "quickAttack" && playerTwoAction == "maneuver")
+        {
+            playerOne.ChangeHealth(0);
+            playerTwo.ChangeHealth(1);
+        }
+        if (playerOneAction == "maneuver" && playerTwoAction == "quickAttack")
+        {
+            playerOne.ChangeHealth(1);
+            playerTwo.ChangeHealth(0);
+        }
+        if (playerOneAction == "strongAttack" && playerTwoAction == "counter")
+        {
+            playerOne.ChangeHealth(2);
+            playerTwo.ChangeHealth(0);
+        }
+        if (playerOneAction == "strongAttack" && playerTwoAction == "parry")
+        {
+            playerOne.ChangeHealth(0);
+            playerTwo.ChangeHealth(0);
+        }
+        if (playerOneAction == "strongAttack" && playerTwoAction == "maneuver")
+        {
+            playerOne.ChangeHealth(0);
+            playerTwo.ChangeHealth(2);
+        }
+        if (playerOneAction == "counter" && playerTwoAction == "counter")
+        {
+            playerOne.ChangeHealth(0);
+            playerTwo.ChangeHealth(0);
+        }
+        if (playerOneAction == "counter" && playerTwoAction == "parry")
+        {
+            playerOne.ChangeHealth(0);
+            playerTwo.ChangeHealth(0);
+        }
+        if (playerOneAction == "counter" && playerTwoAction == "strongAttack")
+        {
+            playerOne.ChangeHealth(0);
+            playerTwo.ChangeHealth(2);
+        }
+        if (playerOneAction == "counter" && playerTwoAction == "maneuver")
+        {
+            // STUN PERSON COUNTERING
+        }
+        if (playerOneAction == "maneuver" && playerTwoAction == "counter")
+        {
+            // STUN PERSON TWO COUNTERING
+            currentState = GameState.ResolutionOne;
+        }
+        if (playerOneAction == "parry" && playerTwoAction == "parry")
+        {
+            playerOne.ChangeHealth(0);
+            playerTwo.ChangeHealth(0);
+        }
+        if (playerOneAction == "parry" && playerTwoAction == "maneuver")
+        {
+            // PERSON TWO CARD DRAW
+            playerOne.ChangeHealth(1);
+            playerTwo.ChangeHealth(1);
+        }
+        if (playerOneAction == "parry" && playerTwoAction == "counter")
+        {
+            // NOTHING
+        }
+        if (playerOneAction == "parry" && playerTwoAction == "strongAttack")
+        {
+            // DISABLE PERSON ONE PARRY
+        }
+        if (playerOneAction == "maneuver" && playerTwoAction == "parry")
+        {
+            // DISABLE PERSON TWO PARRY
+        }
+        if (playerOneAction == "maneuver" && playerTwoAction == "maneuver")
+        {
+            // NOTHING
+        }
+        if (playerOneAction == "maneuver" && playerTwoAction == "strongAttack")
+        {
+            playerOne.ChangeHealth(2);
+            playerTwo.ChangeHealth(0);
+        }
+    }
+    #endregion
 }
